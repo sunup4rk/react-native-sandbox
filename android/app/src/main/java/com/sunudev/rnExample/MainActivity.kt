c9f2949c5d87eb3797d13e51338387ca32d45c0e
박선우 <sunu@bagseon-uui-MacBookAir.local>
2024-03-20 01:16:49 +0900

    package이름 수정

--- android/app/src/main/java/com/sunudev/rnExample/MainActivity.kt
+++ android/app/src/main/java/com/sunudev/rnExample/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.reactnativesandbox
+package com.sunudev.rnExample
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/sunudev/rnExample/MainApplication.kt
+++ android/app/src/main/java/com/sunudev/rnExample/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.reactnativesandbox
+package com.sunudev.rnExample
 
 import android.app.Application
 import com.facebook.react.PackageList
